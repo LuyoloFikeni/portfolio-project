4e974a6615f1ff27147f1c2611db7d71bac29a9f
Luyolo Fikeni <fikeniluyolo@gmail.com>
2024-06-02 14:08:04 +0200

    modified:   db.sqlite3 	modified:   personal_portfolio/__pycache__/settings.cpython-310.pyc 	modified:   personal_portfolio/__pycache__/urls.cpython-310.pyc 	modified:   personal_portfolio/settings.py 	modified:   personal_portfolio/urls.py 	modified:   projects/__pycache__/admin.cpython-310.pyc 	modified:   projects/__pycache__/models.cpython-310.pyc 	modified:   projects/admin.py 	new file:   projects/migrations/0002_project_image.py 	new file:   projects/migrations/__pycache__/0002_project_image.cpython-310.pyc 	modified:   projects/models.py 	modified:   projects/templates/projects/project_detail.html 	modified:   projects/templates/projects/project_index.html

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -12,9 +12,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -125,3 +125,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = BASE_DIR / "uploads/"
+MEDIA_URL = "media/"
